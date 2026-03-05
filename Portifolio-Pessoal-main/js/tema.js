e977e54367cf34a7136403dfbddce140fdb57b7d
Beatriz Omena <beatrizomena18@gmail.com>
2026-03-05 10:50:03 -0300

    atualização de imagens de ajuste na responsividade

--- Portifolio-Pessoal-main/js/tema.js
+++
@@ -1,6 +0,0 @@
-const botaoTema = document.querySelector('.btn-tema');
-
-botaoTema.addEventListener('click', function () {
-    document.body.classList.toggle('tema-escuro');
-    document.body.classList.toggle('tema-claro');
-});
